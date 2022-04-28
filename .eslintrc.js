90655724079699a0702a2a68bda2c45878334c54
Gökhan Demirtaş <gokhan.demirtas@msn.com>
2022-04-29 02:10:58 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  ignorePatterns: "/node_modules",
+  rules: {
+    eqeqeq: ["error", "always"],
+    "no-console": "off",
+    "no-eval": "error",
+    "no-var": "error",
+    "prefer-arrow-callback": "error",
+  },
+};
