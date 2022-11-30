289bab8370e865caa3611cac795bac04a0929f61
322332 <gokhan.demirtas@msn.com>
2022-12-01 01:01:00 +0300

    Refactor test

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -2,36 +2,14 @@ import { subscribe, publish } from "../../index";
 
 describe("Nucleoid synapses", () => {
   it("listens to changes in subjects", () => {
-    const results = [];
-    const expected = [
-      "test2 called",
-      "test1 called",
-      "test1 called",
-      "test3 called",
-      "test3 called",
-      "test1 called",
-    ];
-
-    subscribe("test1", (result) => results.push(result));
-    subscribe("test2", (result) => results.push(result));
-    subscribe("test3", (result) => results.push(result));
-
-    expected.forEach((item) => {
-      publish(item.split(" ")[0], item);
+    subscribe("test1", (result) => {
+      expect(result).toBe("test1");
     });
 
-    expect(results).toEqual(expect.arrayContaining(expected));
-  });
+    subscribe("test1", (result) => {
+      expect(result).toBe("test1");
+    });
 
-  it("publisher won't work in unsubscribe events", () => {
-    const results = [];
-
-    const event = subscribe("test", (result) => results.push(result));
-    publish("test", "test1");
-    expect(results).toEqual(expect.arrayContaining(["test1"]));
-
-    event.unsubscribe();
-    publish("test", "test2");
-    expect(results).toEqual(expect.arrayContaining(["test1"]));
+    publish("test1", "test1");
   });
 });
